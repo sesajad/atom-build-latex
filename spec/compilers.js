9ead7fac694b1729dbf16e1678581fe52042407f
sskahani <ssajad@cafebazaar.ir>
2018-03-09 17:23:54 +0330

    fix compilers

--- spec/compilers.js
+++ spec/compilers.js
@@ -4,13 +4,13 @@ export default {
   list : {
     PDFLaTeX : {
       name: 'PDFLaTeX',
-      cmd: 'pdflatex',
-      arg: '{FILE_ACTIVE_NAME}',
+      exec: 'pdflatex',
+      args: ['{FILE_ACTIVE_NAME}'],
     },
     XeLaTeX: {
       name: 'XeLaTeX',
-      cmd: 'xelatex',
-      arg: '{FILE_ACTIVE_NAME}',
+      exec: 'xelatex',
+      args: ['{FILE_ACTIVE_NAME}'],
     }
   },
   defaultCompiler : 'PDFLaTeX',
